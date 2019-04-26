3765b70e6b1718ef4c97a11f4ea6d432e1bcf4f1
3A532088 <paileo007007@gmail.com>
2019-04-27 05:54:42 +0800

    Initial commit

---
+++ src/main/java/com/zlff/Person.java
@@ -0,0 +1,8 @@
+package com.zlff;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello would");
+    }
+
+}
---
+++ src/main/kotlin/com/zlff/Hello.kt
@@ -0,0 +1,16 @@
+package com.zlff
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//Human().hello()
+    val v=Human()
+    v.hello()
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+
+}
